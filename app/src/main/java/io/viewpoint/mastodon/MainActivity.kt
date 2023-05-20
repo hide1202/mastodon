619b5c7302ed22d3b5dbf0b87347748a9353b0c3
Taegyeong Kim <hide1202@naver.com>
2023-05-20 17:36:34 +0900

    implements to display text contents of timeline

--- app/src/main/java/io/viewpoint/mastodon/MainActivity.kt
+++ app/src/main/java/io/viewpoint/mastodon/MainActivity.kt
@@ -3,16 +3,30 @@ package io.viewpoint.mastodon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.viewpoint.mastodon.api.DefaultMastodonApiClient
+import io.viewpoint.mastodon.api.model.TimelineResponseItem
 import io.viewpoint.mastodon.core.compose.theme.MastodonTheme
+import io.viewpoint.mastodon.feature.timeline.TimelineScreen
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val timelineViewModel: TimelineViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +36,28 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val timelines by timelineViewModel.timelines.collectAsState()
+                    val contents by remember(timelines) {
+                        derivedStateOf {
+                            timelines.map { it.content }
+                        }
+                    }
+                    TimelineScreen(contents)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+class TimelineViewModel : ViewModel() {
+    private val client = DefaultMastodonApiClient()
+    private val _timelines = MutableStateFlow<List<TimelineResponseItem>>(emptyList())
+    val timelines: StateFlow<List<TimelineResponseItem>> = _timelines.asStateFlow()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MastodonTheme {
-        Greeting("Android")
+    init {
+        viewModelScope.launch {
+            val response = client.publicTimelines()
+            _timelines.value = response
+        }
     }
 }
--- feature/timeline/src/main/java/io/viewpoint/mastodon/feature/timeline/TimelineScreen.kt
+++ feature/timeline/src/main/java/io/viewpoint/mastodon/feature/timeline/TimelineScreen.kt
@@ -1,19 +1,30 @@
 package io.viewpoint.mastodon.feature.timeline
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import io.viewpoint.mastodon.core.compose.theme.MastodonTheme
 
 @Composable
-fun TimelineScreen() {
-    Text("Hello, World!")
+fun TimelineScreen(contents: List<String>) {
+    LazyColumn {
+        items(contents) { content ->
+            Text(content)
+        }
+    }
 }
 
 @Preview
 @Composable
-fun TimelineScreenPreview() {
+internal fun TimelineScreenPreview() {
     MastodonTheme {
-        TimelineScreen()
+        TimelineScreen(
+            listOf(
+                "1",
+                "2",
+            )
+        )
     }
 }
